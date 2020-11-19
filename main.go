0244724b22aa5eb822e919ea94126f9b5bcdb62e
lijinghuatongxue <598820383@qq.com>
2020-11-19 23:14:17 +0800

    🐿️：20201119 23:14

--- main.go
+++ main.go
@@ -1,51 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"github.com/lijinghuatongxue/go-test/es/OverallInfo"
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	"net/http"
 )
 
-func GetESOverallInfo(ES_HOST,Port,OverallType string) bool  {
-	type ESInfoStruct struct {
-		Status string
-		Cluster_name string
-		Cluster_uuid string
-	}
-	ip := ES_HOST
-	URL := "/_cluster/stats?pretty"
-
-	url := "http://" + ip + ":" + Port + URL
-	method := "GET"
-
-	client := &http.Client {}
-	req, err := http.NewRequest(method, url, nil)
-
-	if err != nil {
-		logrus.Println(err)
-		return false
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		logrus.Println(err)
-		return false
-	}
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	//if err != nil {
-	//	logrus.Println(err)
-	//	return
-	//}
-	//logrus.Println(string(body))
-	var getESInfo ESInfoStruct
-	json.Unmarshal(body, &getESInfo)
-	logrus.Infof("\n ES-Addr == > %s \n ES-Status == > %s \n ES-name == > %s \n ES-uuid == > %s",ES_HOST+":"+Port,getESInfo.Status,getESInfo.Cluster_name,getESInfo.Cluster_uuid)
-	return true
-	//logrus.Warn(getagentcpuinfo.Node.Successful)
-}
-func main()  {
-	if GetESOverallInfo("192.168.0.253","9200","status") != true{
+func main() {
+	if OverallInfo.GetESOverallInfo("192.168.0.253", "9200", "status") != true {
 		logrus.Error("err")
 	}
 }
