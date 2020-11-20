74b6dbc03868c4a1935bc2e316552ab93b3217c6
lijinghuatongxue <598820383@qq.com>
2020-11-21 00:16:16 +0800

    🐿️：20201121 00:16

--- OverallInfo/EsOverall.go
+++ OverallInfo/EsOverall.go
@@ -12,15 +12,8 @@ func GetESOverallInfo(ES_HOST, Port, OverallType string) bool {
 		Cluster_name string
 		Cluster_uuid string
 	}
-	//logrus.Info(body)
-	//if err != nil {
-	//	logrus.Println(err)
-	//	return
-	//}
-	//logrus.Println(string(body))
 	var getESInfo ESInfoStruct
 	json.Unmarshal(SendHttp.SendHttp(ES_HOST, Port), &getESInfo)
 	logrus.Infof("\n ES-Addr == > %s \n ES-Status == > %s \n ES-name == > %s \n ES-uuid == > %s", ES_HOST+":"+Port, getESInfo.Status, getESInfo.Cluster_name, getESInfo.Cluster_uuid)
 	return true
-	//logrus.Warn(getagentcpuinfo.Node.Successful)
 }
